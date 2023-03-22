6d05c4964e3d09298e4465d51fb7722094693ee9
DarkB0B <miecze123@onet.pl>
2023-03-22 10:13:00 +0100

    eeeee?

---
+++ MovieApitTMDB/Models/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApitTMDB.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public List<string> UserIdList { get; set; } = new List<string>();
+        public int RoomSize { get; set; } //required 2 to start
+        public bool IsStarted { get; set; } //after joining phase ends = true and no one can join
+    }
+}
