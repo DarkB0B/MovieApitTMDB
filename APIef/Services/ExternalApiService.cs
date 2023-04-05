983965131581fbcb4805769004cf88a2c5f58c2e
DarkB0B <miecze123@onet.pl>
2023-04-05 12:57:27 +0200

    Adding Custom Collections

--- APIef/Services/ExternalApiService.cs
+++ APIef/Services/ExternalApiService.cs
@@ -2,14 +2,19 @@
 using Microsoft.AspNetCore.Mvc;
 using APIef.Models;
 using Newtonsoft.Json;
+using APIef.Data;
 
 namespace APIef.Services
 {
     public class ExternalApiService
     {
         private string apiKey = "2c3773c58b96fc195869c5f3162ff399";
-
+        private readonly DataContext _context;
         Deserializer deserializer = new Deserializer();
+        public ExternalApiService(DataContext context)
+        {
+            _context = context;
+        }
 
 
         public async Task<List<Genre>> GetGenres()
@@ -73,30 +78,41 @@ namespace APIef.Services
             List<Movie> movies = new List<Movie>();
             for (int i = 0; i < movieIds.Count; i++)
             {
-
-
-                string id = movieIds[i];
-                var client = new HttpClient();
-                var request = new HttpRequestMessage
+                if (movieIds[i] == null)
                 {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri($"\r\nhttps://api.themoviedb.org/3/movie/{id}?api_key={apiKey}&language=en-US"),
-                };
-
-                using (var response = await client.SendAsync(request))
+                    continue;
+                }
+                Movie? moviedb = await _context.Movies.FindAsync(movieIds[i]);
+                if (moviedb == null)
                 {
-                    response.EnsureSuccessStatusCode();
-                    var body = await response.Content.ReadAsStringAsync();
-                    var result = JsonConvert.DeserializeObject<dynamic>(body);
-                    if (result == null)
+
+                    string id = movieIds[i];
+                    var client = new HttpClient();
+                    var request = new HttpRequestMessage
                     {
-                        throw new ArgumentNullException("API returned null");
+                        Method = HttpMethod.Get,
+                        RequestUri = new Uri($"\r\nhttps://api.themoviedb.org/3/movie/{id}?api_key={apiKey}&language=en-US"),
+                    };
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var body = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<dynamic>(body);
+                        if (result == null)
+                        {
+                            throw new ArgumentNullException("API returned null");
+                        }
+                        Movie? movie = deserializer.DeserializeMovie(result);
+                        if (movie != null)
+                        {
+                            movies.Add(movie);
+                        }
                     }
-                    Movie? movie = deserializer.DeserializeMovie(result);
-                    if(movie != null) 
-                    { 
-                        movies.Add(movie);
-                    }
+                }
+                else
+                {
+                    movies.Add(moviedb);
                 }
             }
             return movies;
